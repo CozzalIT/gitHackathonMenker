8ad87821b452e7c168c6de5550a55cd8c560fd7b
abulaiz <ronabulaiz@gmail.com>
2018-06-30 06:37:45 +0700

    Lancar Jaya

---
+++ modules/beranda/view/info.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<?php
+  require('../../../template/head.php');
+  $thisPage = 'Beranda';
+?>
+
+<body class="home-one" data-spy="scroll" data-target=".mainmenu-area" data-offset="90">
+
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!--- PRELOADER -->
+    <div class="preeloader">
+        <div class="preloader-spinner"></div>
+    </div>
+
+    <!--SCROLL TO TOP-->
+    <a href="#home" class="scrolltotop"><i class="fa fa-long-arrow-up"></i></a>
+
+    <!--START TOP AREA-->
+    <header class="" id="home">
+        <div class="header-top-area">
+            <!--MAINMENU AREA-->
+            <?php
+              require('../../../template/navbar-modules.php');
+            ?>
+            <!--END MAINMENU AREA END-->
+        </div>
+        <?php
+          //require('../../../template/welcome.php');
+        ?>
+    </header>
+    <!--END TOP AREA-->
+
+    <!--APP AREA-->
+    <section class="app-area relative padding-100-50 sky-gray-bg" id="app">
+        <div class="app-area-mockup-bg"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-lg-6 col-sm-12 col-xs-12">
+                    <div class="area-title wow fadeIn">
+                        <h2>Bekraf Developer Day 2018 - Surabaya</h2>
+                        <span class="icon-and-border"><i class="material-icons">phone_android</i></span>
+                        <p>Praktisi aplikasi dan game akan berkumpul di Surabaya - memberikan bimbingan kepada developer untuk kemandirian bangsa.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                  <!-- Column -->
+                  <div class="col-lg-12 col-xlg-12 col-md-12">
+                      <div class="card card-bg pad-bot-card">
+                          <!-- Tab panes -->
+                          <div class="tab-content">
+                              <!--second tab-->
+                              <div class="tab-pane active" id="Deskripsi" role="tabpanel">
+                                  <div class="card-block">
+                                      <p class="m-t-30">Badan Ekonomi Kreatif (BEKRAF) akan memberikan beasiswa kelas Dicoding Academy Belajar Membuat Aplikasi Android untuk Pemula atau Belajar Membuat Game untuk Pemula secara gratis. Untuk anda yang berminat untuk mendapatkan beasiswa tersebut, silakan melakukan tahapan berikut :
+                                      <ul>
+                                      <li>Mengisi form pendaftaran di link berikut : http://bit.ly/pendaftaranbeasiswabddsurabaya2018</li>
+                                       <li>Mencetak dan mengisi surat  permohonan yang dikumpulkan pada saat registrasi di BDD Surabaya.<li> <li>Format surat permohonan dapat di download di link : http://bit.ly/beasiswabddsurabaya2018</li>
+                                      </ul>
+                                        Jika memiliki pertanyaan, silakan email ke bdd@dicoding.com
+                                        </p>
+                                        <p class="m-t-30">
+Acara bersifat GRATIS dan dimulai pukul 07.30 - 18.00 WIB. Akomodasi dan Transportasi ditanggung mandiri oleh masing-masing peserta
+
+<br>Susunan Acara <br>
+<ol type="1">
+<li>    Registrasi ulang (07.30 - 08.00) </li>
+<li>    General Session (08.00 - 12.00) </li>
+<li>     Ishoma (12.00 - 12.30) </li>
+<li>     Paralel (12.30 - 15.00)</li>
+<li>    Ishoma (15.00 - 15.30) </li>
+<li>    Paralel lanjutan (15.30 - 18.00) </li>
+<li>    Pembagian Merchandise (18.00 - selesai) </li>
+</ol> <br>
+
+Apabila anda memiliki portfolio Aplikasi Mobile (Android, iOS, Tizen, Windows), Web, Desktop, ataupun IoT, dapatkan kesempatan untuk pameran hasil karya anda dengan mengikuti Challenge Made in Surabaya -  https://www.dicoding.com/challenges/401 hingga 25 Juni 2018.
+
+
+Terdapat pendaftaran dan konsultasi Hak Kekayaan Intelektual dari Deputi HKI BEKRAF untuk karya kreatif. Silakan kunjungi booth BEKRAF pada hari pelaksanaan acara untuk mendapatkan informasinya.
+
+                                      </p>
+                                  </div>
+                              </div>
+                              <div class="tab-pane" id="Dokumentasi" role="tabpanel">
+                                  <div class="card-block">
+                                    <div class="row mrgn-event">
+                                        <div class="col-lg-3 col-md-6 m-b-20"><img src="../../../assets/img/dok/1.jpg" alt="user" class="img-responsive radius"></div>
+                                        <div class="col-lg-3 col-md-6 m-b-20"><img src="../../../assets/img/dok/2.jpg" alt="user" class="img-responsive radius" style="height: 160px;"></div>
+                                        <div class="col-lg-3 col-md-6 m-b-20"><img src="../../../assets/img/dok/1.jpg" alt="user" class="img-responsive radius" style="height: 160px;"></div>
+                                        <div class="col-lg-3 col-md-6 m-b-20"><img src="../../../assets/img/dok/2.jpg" alt="user" class="img-responsive radius" style="height: 160px;"></div>
+                                    </div>
+                                  </div>
+                              </div>
+                              <div class="tab-pane" id="Diskusi" role="tabpanel">
+                                  <div class="card-block">
+                                    <form class="form-horizontal form-material mrgn-event">
+                                        <div class="form-group" style="margin-bottom: 10px;">
+                                            <h4 class="col-md-12">Bergabung dalam diskusi</h4>
+                                            <div class="col-md-12" style="background:white;">
+                                                <textarea rows="5" class="form-control form-control-line" style="padding-top: 10px;padding-left: 0px;"></textarea>
+                                            </div>
+                                            <div class="like-comm m-t-20"> <a href="javascript:void(0)" class="link m-r-10"><i class="fa fa-camera"></i> Sisipkan gambar</a> <a href="javascript:void(0)" class="link m-r-10"><i class="fa fa-paperclip"></i> Sisipkan file</a> </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-10">
+                                            </div>
+                                            <div class="col-sm-2">
+                                                <button class="btn btn-success">Bagikan</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <div class="sl-item mrgn-event">
+                                        <div class="sl-right">
+                                            <div> <a href="#" class="link">John Doe</a> <span class="sl-date">5 minutes ago</span>
+                                                <div class="m-t-20 row">
+                                                    <div class="col-md-9 col-xs-12">
+                                                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. </p> </div>
+                                                </div>
+                                                <div class="like-comm m-t-20"> <a href="javascript:void(0)" class="link m-r-10">2 comment</a> <a href="javascript:void(0)" class="link m-r-10"><i class="fa fa-heart text-danger"></i> 5 Love</a> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="sl-item mrgn-event">
+                                        <div class="sl-right">
+                                            <div><a href="#" class="link">John Doe</a> <span class="sl-date">5 minutes ago</span>
+                                                <p class="m-t-10"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper </p>
+                                            </div>
+                                            <div class="like-comm m-t-20"> <a href="javascript:void(0)" class="link m-r-10">2 comment</a> <a href="javascript:void(0)" class="link m-r-10"><i class="fa fa-heart text-danger"></i> 5 Love</a> </div>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                  </div>
+                              </div>
+                              <button style="width: 200px;" class="btn btn-success btn-event col-sm-1">Ajukan Pertanyaan</button>
+                          </div>
+                          <div class="row">
+                            <button class="btn btn-success btn-event col-sm-1">Ikuti Acara</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+        </div>
+    </section>
+    <!--APP AREA END-->
+
+    <!--====== SCRIPTS JS ======-->
+    <script src="../../../assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="../../../assets/js/vendor/bootstrap.min.js"></script>
+
+    <!--====== PLUGINS JS ======-->
+    <script src="../../../assets/js/vendor/jquery.easing.1.3.js"></script>
+    <script src="../../../assets/js/vendor/jquery-migrate-1.2.1.min.js"></script>
+    <script src="../../../assets/js/vendor/jquery.appear.js"></script>
+    <script src="../../../assets/js/owl.carousel.min.js"></script>
+    <script src="../../../assets/js/slick.min.js"></script>
+    <script src="../../../assets/js/stellar.js"></script>
+    <script src="../../../assets/js/wow.min.js"></script>
+    <script src="../../../assets/js/jquery-modal-video.min.js"></script>
+    <script src="../../../assets/js/stellarnav.min.js"></script>
+    <script src="../../../assets/js/contact-form.js"></script>
+    <script src="../../../assets/js/jquery.ajaxchimp.js"></script>
+    <script src="../../../assets/js/jquery.sticky.js"></script>
+
+    <!--===== ACTIVE JS=====-->
+    <script src="../../../assets/js/main.js"></script>
+</body>
+
+</html>
